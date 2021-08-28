4734fbc70d580cce576bcb593b5168905f4b9cbf
Fernando Saraiva <fpsaraiva@gmail.com>
2021-08-28 17:19:49 -0300

    desafio semana 05 - order service

---
+++ src/main/java/br/com/codenation/service/OrderServiceImpl.java
@@ -0,0 +1,54 @@
+package br.com.codenation.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import br.com.codenation.model.OrderItem;
+import br.com.codenation.model.Product;
+import br.com.codenation.repository.ProductRepository;
+import br.com.codenation.repository.ProductRepositoryImpl;
+
+public class OrderServiceImpl implements OrderService {
+
+	private ProductRepository productRepository = new ProductRepositoryImpl();
+
+	/**
+	 * Calculate the sum of all OrderItems
+	 */
+	@Override
+	public Double calculateOrderValue(List<OrderItem> items) {
+		return items.stream().map(i -> {
+            Product product = productRepository.findById(i.getProductId()).get();
+            Double value = product.getIsSale() ? product.getValue() * .8 : product.getValue();
+            return i.getQuantity() * value;
+        }).reduce(0.0, Double::sum);
+	}
+
+	/**
+	 * Map from idProduct List to Product Set
+	 */
+	@Override
+	public Set<Product> findProductsById(List<Long> ids) {
+		return ids.stream().map(id -> productRepository.findById(id)).filter(Optional::isPresent).map(Optional::get)
+				.collect(Collectors.toSet());
+	}
+
+	/**
+	 * Calculate the sum of all Orders(List<OrderIten>)
+	 */
+	@Override
+	public Double calculateMultipleOrders(List<List<OrderItem>> orders) {
+		return orders.stream().map(this::calculateOrderValue).reduce(0.0, Double::sum);
+	}
+
+	/**
+	 * Group products using isSale attribute as the map key
+	 */
+	@Override
+	public Map<Boolean, List<Product>> groupProductsBySale(List<Long> productIds) {
+		return findProductsById(productIds).stream().collect(Collectors.groupingBy(Product::getIsSale));
+	}
+}
